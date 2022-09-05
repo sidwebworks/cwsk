f1fef6aac7bde2134cae1ea666c4179366905dec
Sameer Khan <86143326+SK07860@users.noreply.github.com>
2022-09-05 12:59:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+import express from "express";
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome</h1>")
 })
 app.listen(process.env.PORT || 3000)
