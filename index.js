e50b07e4c5bbee01dfb94755c875d932b0d64830
Sameer <jayadakhan0786@gmail.com>
2022-09-06 15:36:17 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome</h1>")
-})
-app.listen(process.env.PORT || 3000)
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To CWSK</h1>");
+});
+app.listen(process.env.PORT || 3000);
