f88e1b3090fbedd020ad4408912a591e9084548d
Sameer <jayadakhan0786@gmail.com>
2022-09-06 16:07:12 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import nunjucks from "nunjucks";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+nunjucks.configure(resolve(__dirname, "templates"), {
+  express: app,
+  autoscape: true,
+  noCache: false,
+  watch: true,
+});
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome To CWSK</h1>");
+app.get("/", function (req, res) {
+  res.render("./pages/home.html");
 });
+
+app.use(express.static("public"));
+
 app.listen(process.env.PORT || 3000);
