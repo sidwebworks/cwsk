450fcfc1d50a8c8bf65a6882d947e8be0e571658
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-09-09 17:49:22 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-import express from "express";
-import nunjucks from "nunjucks";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import express from 'express';
+import nunjucks from 'nunjucks';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = express();
-nunjucks.configure(resolve(__dirname, "templates"), {
+
+nunjucks.configure(resolve(__dirname, 'templates'), {
   express: app,
   autoscape: true,
   noCache: false,
 });
 
-app.get("/", function (req, res) {
-  console.log("A New request encoutered", req.ip);
-  res.render("./pages/home.html");
+app.get('/', function (req, res) {
+  console.log('A New request encoutered', req.ip);
+  res.render('./pages/home.html');
 });
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
